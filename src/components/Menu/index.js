75a1b6534385f2c1f4b0f4072c8d834fe6bc4d44
SimonGzAppspring <gonzalezsimon18@gmail.com>
2022-02-27 22:41:18 -0500

    [update] some style updates

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Menu as SemanticMenu, Container, Image } from 'semantic-ui-react';
-import logo from '../../statics/images/logo_v4.svg';
+import logo from '../../statics/images/pokeball.svg';
 
 const Menu = () => {
   return (
     <SemanticMenu fixed='top'>
       <Container>
         <SemanticMenu.Item to='/'>
-          <Image size='small' src={logo} />
+          <Image size='mini' src={logo} />
         </SemanticMenu.Item>
         <SemanticMenu.Item to='/'>Home</SemanticMenu.Item>
         <SemanticMenu.Item to='/' position='right'>
